ca34c6bd937f5c8e9124164820ab14b025cb5bb6
ᏰᎡᏫᏫᏦ ᏚᎯᎷᎯ <111586917+BrookSama@users.noreply.github.com>
2023-06-21 17:11:09 +0300

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -4,9 +4,9 @@ export function before(m) {
     let user = global.db.data.users[m.sender]
     if (user.afk > -1) {
         m.reply(`
-  ✅ You stopped being AFK 
-${user.afkReason ? ' \n▢ *Reason :* ' + user.afkReason : ''}
-▢ *AFK Duration :* ${(new Date - user.afk).toTimeString()}
+  ✅ لقد توقفت عن الإختفاء 
+${user.afkReason ? ' \n▢ *السبب :* ' + المستخدم.afkReason : ''}
+▢ *الإختفاء مدة :* ${(new Date - user.afk).toTimeString()}
   `.trim())
         user.afk = -1
         user.afkReason = ''
@@ -21,10 +21,10 @@ ${user.afkReason ? ' \n▢ *Reason :* ' + user.afkReason : ''}
             continue
         let reason = user.afkReason || ''
         m.reply(`
-💤 The human u mentioned is afk 
+💤 الشخص الذي منشنته في حالة إختفاء 
 
-${reason ? '▢ *Reason* : ' + reason : '▢ *Reason* : Without reason'}
-▢ *AFK Duration :* ${(new Date - afkTime).toTimeString()}
+${reason ? '▢ *السبب* : ' + reason : '▢ *السبب* : Without reason'}
+▢ *الإختفاء مدة :* ${(new Date - afkTime).toTimeString()}
   `.trim())
     }
     return true
