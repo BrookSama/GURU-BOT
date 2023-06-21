88617651c06658bf6c72298e57ad5b6329ad471b
ᏰᎡᏫᏫᏦ ᏚᎯᎷᎯ <111586917+BrookSama@users.noreply.github.com>
2023-06-22 00:53:54 +0300

    Update gp-warns.js

--- plugins/gp-warns.js
+++ plugins/gp-warns.js
@@ -1,7 +1,7 @@
 
 let handler = async (m, { conn, args, groupMetadata}) => {
        let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-       if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+       if (!(who in global.db.data.users)) throw `✳️ لم يتم العثور على المستخدم في قاعدة البيانات الخاصة بي`
        let warn = global.db.data.users[who].warn
        let name = conn.getName(who)
       m.reply(`
