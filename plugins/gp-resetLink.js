1715013f0312d0b856432074917d5177cca04ff3
ᏰᎡᏫᏫᏦ ᏚᎯᎷᎯ <111586917+BrookSama@users.noreply.github.com>
2023-06-25 15:26:42 +0300

    Update gp-resetLink.js

--- plugins/gp-resetLink.js
+++ plugins/gp-resetLink.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
 }
 handler.help = ['resetlink']
 handler.tags = ['group']
-handler.command = ['revoke', 'resetlink', 'anularlink'] 
+handler.command = ['revoke', 'resetlink', 'anularlink', 'اعادة-تعيين'] 
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
