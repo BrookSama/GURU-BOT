753af8133fd776ef8baf86bdc0c0792b30cea54e
ᏰᎡᏫᏫᏦ ᏚᎯᎷᎯ <111586917+BrookSama@users.noreply.github.com>
2023-06-25 15:22:15 +0300

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`▢ قروب : *${groupMetadata.subject}*\n▢ الاعضاء : *${participants.length}*${text ? `\n▢ الرسالة : ${text}\n` : ''}\n┌───⊷ *منشن جماعي*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n𝐓𝐀𝐌𝐀𝐊𝐈 𝐁𝐎𝐓', null, {
+    m.reply(`☠️ قروب : *${groupMetadata.subject}*\n☠️ الاعضاء : *${participants.length}*${text ? `\n☠️ الرسالة : ${text}\n` : ''}\n *منشن جماعي*\n` + users.map(v => '☠️ @' + v.replace(/@.+/, '')).join`\n` + '\n𝐓𝐀𝐌𝐀𝐊𝐈 𝐁𝐎𝐓', null, {
         mentions: users
     })
 }
 
 handler.help = ['tagall']
 handler.tags = ['group']
-handler.command = ['tagall']
+handler.command = ['tagall' ,'منشن']
 handler.admin = true
 handler.group = true
 
