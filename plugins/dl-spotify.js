3d232f4e6daf3de20ca3f2e23e994b3011c8b3a0
ᏰᎡᏫᏫᏦ ᏚᎯᎷᎯ <111586917+BrookSama@users.noreply.github.com>
2023-06-25 15:59:33 +0300

    Update dl-spotify.js

--- plugins/dl-spotify.js
+++ plugins/dl-spotify.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async(m, { conn, text }) => {
-if (!text) throw `*ENTER NAME OF SONG*`
+if (!text) throw `*أدخل اسم الأغنية*`
 try {
 let res = await fetch(`https://api.lolhuman.xyz/api/spotifysearch?apikey=${lolkeysapi}&query=${text}`)
 let json = await res.json()
@@ -8,12 +8,12 @@ let { link } = json.result[0]
 let res2 = await fetch(`https://api.lolhuman.xyz/api/spotify?apikey=${lolkeysapi}&url=${link}`)
 let json2 = await res2.json()
 let { thumbnail, title, artists } = json2.result
-let spotifyi = `❒═════❬ 𝐒𝐏𝐎𝐓𝐈𝐅𝐘 ❭═════╾❒\n┬\n├‣✨ *TITLE:* ${title}\n┴\n┬\n├‣🗣️ *ARTIST:* ${artists}\n┴\n┬\n├‣🌐 *𝚄𝚁𝙻*: ${link}\n┴\n┬\n├‣💚 *SEARCH URL:* ${json2.result.link}\n┴`
+let spotifyi = `❒═════❬ سبوتيفاي ❭═════╾❒\n┬\n├‣✨ *العنوان:* ${title}\n┴\n┬\n├‣🗣️ *الفنان:* ${artists}\n┴\n┬\n├‣🌐 *الرابط*: ${link}\n┴\n┬\n├‣💚 *رابط البحث:* ${json2.result.link}\n┴`
 conn.sendFile(m.chat, thumbnail, 'error.jpg', spotifyi, m)
 let aa = await conn.sendMessage(m.chat, { audio: { url: json2.result.link }, fileName: `error.mp3`, mimetype: 'audio/mp4' }, { quoted: m })  
 if (!aa) return conn.sendFile(m.chat, json2.result.link, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' }) 
 } catch {
-throw '* 𝙴𝚁𝚁𝙾𝚁*'
+throw '* خطأ*'
 }}
-handler.command = /^(spotify|song)$/i
+handler.command = /^(spotify|song |سبوتيفاي)$/i
 export default handler
