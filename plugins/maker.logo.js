d92d4cf76229a4145307e1e6b71924696e99843d
ᏰᎡᏫᏫᏦ ᏚᎯᎷᎯ <111586917+BrookSama@users.noreply.github.com>
2023-06-25 15:15:21 +0300

    Update maker.logo.js

--- plugins/maker.logo.js
+++ plugins/maker.logo.js
@@ -2,33 +2,33 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 	 let name = conn.getName(m.sender)
     let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg')
     let user = global.db.data.users[m.sender]
-   let tee = `✳️ Enter a short text\n\n📌 Example  : *${usedPrefix + command}* GURU`
-   let too = `✳️ Separate the text with a *+* \n\n📌 Example : \n*${usedPrefix + command}* GURU *+* BOT`
+   let tee = `✳️ اكتب شيء مع الامر\n\n📌 مثال  : *${usedPrefix + command}* Tamaki`
+   let too = `✳️ قم بفصل النص ب *+* \n\n📌 مثال : \n*${usedPrefix + command}* Tamaki *+* BOT`
     m.react(rwait)
 let type = (command).toLowerCase()
 switch (type) {
     case 'marvel': 
 	if (!text) throw tee
 	let di = global.API('fgmods', '/api/textpro/marvel', { text }, 'apikey')
-	conn.sendFile(m.chat, di, 'logo.png', `✅ Result`, m)
+	conn.sendFile(m.chat, di, 'logo.png', `✅ نتيجة`, m)
 	m.react(done)
 	break 
      case 'ninja': 
 	if (!text) throw tee
 	let mh = global.API('fgmods', '/api/textpro/ninja', { text }, 'apikey')
-	conn.sendFile(m.chat, mh, 'logo.png', `✅ Result`, m)
+	conn.sendFile(m.chat, mh, 'logo.png', `✅ نتيجة`, m)
 	m.react(done)
 	break 
      case 'tiktok': 
 	if (!text) throw tee
 	let ch = global.API('fgmods', '/api/textpro/tiktok', { text }, 'apikey')
-	conn.sendFile(m.chat, ch, 'logo.png', `✅ Result`, m)
+	conn.sendFile(m.chat, ch, 'logo.png', `✅ نتيجة`, m)
 	m.react(done)
 	break  
      case 'lightglow': 
 	if (!text) throw tee
 	let ut = global.API('fgmods', '/api/textpro/lightglow', { text }, 'apikey')
-	conn.sendFile(m.chat, ut, 'logo.png', `✅ Result`, m)
+	conn.sendFile(m.chat, ut, 'logo.png', `✅ نتيجة`, m)
 	m.react(done)
 	break 
      case 'layer': 
@@ -36,19 +36,19 @@ switch (type) {
 	if (!text.includes('+')) throw too  
 	let [a, b] = text.split`+`   
 	let lr = global.API('fgmods', '/api/textpro/layered', { text: a, text2: b}, 'apikey')
-	conn.sendFile(m.chat, lr, 'logo.png', `✅ Result`, m)
+	conn.sendFile(m.chat, lr, 'logo.png', `✅ نتيجة`, m)
 	m.react(done)
 	break  
      case 'aglow': 
 	if (!text) throw tee
 	let da = global.API('fgmods', '/api/textpro/advancedglow', { text }, 'apikey')
-	conn.sendFile(m.chat, da, 'logo.png', `✅ Result`, m)
+	conn.sendFile(m.chat, da, 'logo.png', `✅ نتيجة`, m)
 	m.react(done)
 	break 
      case 'diamond': 
 	if (!text) throw tee
 	let de = global.API('fgmods', '/api/textpro/diamond', { text }, 'apikey')
-	conn.sendFile(m.chat, de, 'logo.png', `✅ Result`, m)
+	conn.sendFile(m.chat, de, 'logo.png', `✅ نتيجة`, m)
 	m.react(done)
 	break 
   default:
