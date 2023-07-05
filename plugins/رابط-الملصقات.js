1bb75c5bb3e8d7331a197cd66205246fe7c32069
ᏰᎡᏫᏫᏦ ᏚᎯᎷᎯ <111586917+BrookSama@users.noreply.github.com>
2023-07-05 12:28:06 +0300

    Create رابط-الملصقات.js

---
+++ plugins/رابط-الملصقات.js
@@ -0,0 +1,35 @@
+import fetch from 'node-fetch'
+
+let handler = async function (m, { conn, text, usedPrefix }) {
+  
+let m2 =`*﷽*
+
+*––––––––– ·「🍡」· –––––––––––*
+
+* ① قروب ستيكرات نشر يومي🍡*
+
+* ➁ حمل تطبيق " sticker maker “ عشان تقدر تحمل الحزم*
+
+* ➂ ممنوع طلب الإشراف 💗*
+
+*➃ يمنع إزعاج الأعضاء في الخاص*
+
+*––––––––– ·「🍡」· –––––––––––*
+*⑤ رابط القروب :『 https://chat.whatsapp.com/DV9ME7ocNPJA03qXmezoeI 』*
+
+*تابع لمملكة*
+
+* 『☯︎ 𝐎𝐓𝐀𝐊𝐔「☠️」𝐊𝐈𝐍𝐆𝐃𝐎𝐌 ♕︎』* `
+
+
+//const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
+    let pp = 'Guru2.jpg' 
+    conn.reply(m.chat, m2,null,m)
+   
+}
+
+handler.help = ['audios']
+handler.tags = ['main']
+handler.command = ['ستيكرات','الاستيكرات'] 
+
+export default handler
