9a5bac34f513318a66d48874bc475a575360f825
ᏰᎡᏫᏫᏦ ᏚᎯᎷᎯ <111586917+BrookSama@users.noreply.github.com>
2023-07-10 17:21:04 +0300

    Update owner-un-block.js

--- plugins/owner-un-block.js
+++ plugins/owner-un-block.js
@@ -1,5 +1,5 @@
 let handler = async (m, { text, conn, usedPrefix, command }) => {
-    let why = `*ERROR, EXAMPLE:*\n*—◉ ${usedPrefix + command} @${m.sender.split("@")[0]}*`
+    let why = `*خَطَأ, مثال:*\n*—◉ ${usedPrefix + command} @${m.sender.split("@")[0]}*`
     let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false
     if (!who) conn.reply(m.chat, why, m, { mentions: [m.sender] })
     let res = [];
@@ -15,10 +15,10 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
         else conn.reply(m.chat, why, m, { mentions: [m.sender] })
         break;
     }
-    if (res[0]) conn.reply(m.chat, `*SUCCESS! USER ${command} ACTION PERFORMED ON ${res ? `${res.map(v => '@' + v.split("@")[0])}` : ''}*`, m, { mentions: res })
+    if (res[0]) conn.reply(m.chat, `*النجاح! المستخدم ${command} الإجراء الذي تم تنفيذه على ${res ? `${res.map(v => '@' + v.split("@")[0])}` : ''}*`, m, { mentions: res })
   }
   
-  handler.command = /^(block|unblock)$/i
+  handler.command = /^(block|unblock|انبلوك)$/i
   handler.rowner = true
   export default handler
   
