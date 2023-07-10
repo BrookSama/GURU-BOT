f6b6675a0dc9251f90e8b8003685be25d447cfe8
ᏰᎡᏫᏫᏦ ᏚᎯᎷᎯ <111586917+BrookSama@users.noreply.github.com>
2023-07-10 17:18:48 +0300

    Update owner-banUser.js

--- plugins/owner-banUser.js
+++ plugins/owner-banUser.js
@@ -5,18 +5,18 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) throw `✳️ Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @user`
+    if (!who) throw `✳️ ضع رقم أو منشن شخصا ما\n\n📌 مثال : ${usedPrefix + command} @user`
     let users = global.db.data.users
     users[who].banned = true
     conn.reply(m.chat, `
-✅ BANNED
+✅ المحظورين
 
 ───────────
 @${who.split`@`[0]} you will no longer be able to use my commands `, m, { mentions: [who] })
 }
 handler.help = ['ban @user']
 handler.tags = ['owner']
-handler.command = /^ban$/i
+handler.command = /^ban|بلوك$/i
 handler.rowner = true
 
 export default handler
