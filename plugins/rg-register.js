5ead4e689b8d32cabdc92fd5d93e2172e86d93cb
ᏰᎡᏫᏫᏦ ᏚᎯᎷᎯ <111586917+BrookSama@users.noreply.github.com>
2023-07-10 16:21:21 +0300

    Update rg-register.js

--- plugins/rg-register.js
+++ plugins/rg-register.js
@@ -5,25 +5,25 @@ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) throw `✳️ You are already registered\n\nDo you want to re-register?\n\n 📌 Use this command to remove your record \n*${usedPrefix}unreg* <Serial number>`
-  if (!Reg.test(text)) throw `⚠️ Format incorrect\n\n ✳️ Use this command: *${usedPrefix + command} name.age*\n📌Exemple : *${usedPrefix + command}* ${name2}.16`
+  if (user.registered === true) throw `✳️ أنت مسجل بالفعل\n\nهل تريد إعادة التسجيل؟\n\n 📌 استخدم هذا الأمر لإزالة سجلك \n*${usedPrefix}unreg* <Serial number>`
+  if (!Reg.test(text)) throw `⚠️ تنسيق غير صحيح\n\n ✳️ استخدم هذا الأمر: *${usedPrefix + command} الاسم.العمر*\n📌مِثال : *${usedPrefix + command}* ${name2}.16`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw '✳️ The name cannot be empty'
-  if (!age) throw '✳️ age cannot be empty'
-  if (name.length >= 30) throw '✳️The name is too long' 
+  if (!name) throw '✳️ لا يمكن أن يكون الاسم فارغا'
+  if (!age) throw '✳️ لا يمكن أن يكون العمر فارغا'
+  if (name.length >= 30) throw '✳️الاسم طويل جدا' 
   age = parseInt(age)
-  if (age > 100) throw '👴🏻 Wow grandpa wants to play bot'
-  if (age < 5) throw '🚼  there is a grandpa baby jsjsj '
+  if (age > 100) throw '👴🏻 واو جدي يريد أن يلعب دور البوت'
+  if (age < 5) throw '🚼  هناك طفل يا جد جاجاجا'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`
-┌─「 *REGISTERED* 」─
-▢ *NUMBER:* ${name}
-▢ *AGE* : ${age} years
-▢ *SERIEL NUMBER* :
+┌─「 *مسجل* 」─
+▢ *الرقم:* ${name}
+▢ *العمر* : ${age} years
+▢ *الرقم التسلسلي* :
 ${sn}
 └──────────────
 
@@ -33,7 +33,7 @@ ${sn}
 handler.help = ['reg'].map(v => v + ' <name.age>')
 handler.tags = ['rg']
 
-handler.command = ['verify', 'reg', 'register', 'registrar'] 
+handler.command = ['verify', 'reg', 'register', 'registrar', 'تسجيل'] 
 
 export default handler
 
