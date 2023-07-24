e514c64595ae474df4c84603fdb649e74258fff2
ᏰᎡᏫᏫᏦ ᏚᎯᎷᎯ <111586917+BrookSama@users.noreply.github.com>
2023-07-24 18:20:01 +0300

    Update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -3,7 +3,7 @@ import { youtubeSearch } from '@bochilteam/scraper'
 import yts from 'yt-search'
 let handler = async(m, { conn, usedPrefix, text, args, command }) => {
 
-    if (!text) throw `✳️ Enter a song title\n\n*📌 Example*\n*${usedPrefix + command}* Lil Peep hate my fuccn life `
+    if (!text) throw `✳️ أكتب اسم الأغنية\n\n*📌 مثال*\n*${usedPrefix + command}* Lil Peep hate my fuccn life `
     m.react('📀')
     let result = await yts(text)
     let ytres = result.all
@@ -14,10 +14,10 @@ let handler = async(m, { conn, usedPrefix, text, args, command }) => {
           ['🎥 MP4', `${usedPrefix}fgmp4 ${v.url}`, `▢ ⌚ *Duratión:* ${v.timestamp}\n▢ 👀 *Views:* ${v.views}\n▢ 📌 *Títle* : ${v.title}\n▢ 📆 *Publiced:* ${v.ago}\n`]
         ]])
 	})
-	return conn.sendList(m.chat, '  ≡ *GURU MUSIC*🔎', `\n 📀 Here a list of results from :\n *${text}*`, igfg, `Click `, listSections, m)
+	return conn.sendList(m.chat, '  ≡ *𝐓𝐀𝐌𝐀𝐊𝐈 MUSIC*🔎', `\n 📀 فيما يلي قائمة بالنتائج من :\n *${text}*`, igfg, `Click `, listSections, m)
 }
 handler.help = ['play2']
 handler.tags = ['dl']
-handler.command = ['play2', 'playvid2', 'playlist', 'playlista'] 
+handler.command = ['play2', 'playvid2', 'playlist', 'playlista', 'يوتيوب2'] 
 
 export default handler
