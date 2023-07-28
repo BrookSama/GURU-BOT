b206fe85a167d72060da4abc21b44d4872d5b6e7
ᏰᎡᏫᏫᏦ ᏚᎯᎷᎯ <111586917+BrookSama@users.noreply.github.com>
2023-07-29 00:46:56 +0300

    Update game-ppt.js

--- plugins/game-ppt.js
+++ plugins/game-ppt.js
@@ -2,45 +2,45 @@
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let poin = 300
-    let reseqv = `✳️ Select rock/paper/scissors\n\nExample : *${usedPrefix + command}* paper\n`
+    let reseqv = `✳️ اختر حجرة/ورق/مقص\n\nمثال : *${usedPrefix + command}* ورقة\n`
     if (!text) throw reseqv
     var astro = Math.random()
 
     if (astro < 0.34) {
-        astro = 'rock'
+        astro = 'حجرة'
     } else if (astro > 0.34 && astro < 0.67) {
-        astro = 'scissors'
+        astro = 'مقص'
     } else {
-        astro = 'paper'
+        astro = 'ورقة'
     }
 
 
     if (text == astro) {
       global.db.data.users[m.sender].exp += 100
-        m.reply(`▢ *A tie*\n\n‣ You : ${text}\n‣ Guru : ${astro}\n\n🎁 Points (±)100 XP`)
-    } else if (text == 'rock') {
-        if (astro == 'scissors') {
+        m.reply(`▢ *تعادل*\n\n‣ انت : ${text}\n‣ تاماكي : ${astro}\n\n🎁 النقاط (±)100 XP`)
+    } else if (text == 'حجرة') {
+        if (astro == 'مقص') {
             global.db.data.users[m.sender].exp += 300
-            m.reply(`▢ *Won* 🎊\n\n‣ You : ${text}\n‣ GURU : ${astro}\n\n🎁 Points *+${poin} XP*`)
+            m.reply(`▢ *الفائز* 🎊\n\n‣ انت : ${text}\n‣ تاماكي : ${astro}\n\n🎁 النقاط *+${poin} XP*`)
         } else {
           global.db.data.users[m.sender].exp -= 300
-            m.reply(`▢ *lost*\n\n‣ You : ${text}\n‣ GURU : ${astro}\n\n Points *-${poin} XP*`)
+            m.reply(`▢ *الخاسر*\n\n‣ انت : ${text}\n‣ تاماكي : ${astro}\n\n النقاط *-${poin} XP*`)
         }
-    } else if (text == 'scissors') {
-        if (astro == 'paper') {
+    } else if (text == 'مقص') {
+        if (astro == 'ورقة') {
             global.db.data.users[m.sender].exp += 300
-            m.reply(`▢ *won* 🎊\n\n‣ You : ${text}\n‣ GURU : ${astro}\n\n🎁 Points *+${poin} XP*`)
+            m.reply(`▢ *الفائز* 🎊\n\n‣ انت : ${text}\n‣ تاماكي : ${astro}\n\n🎁 النقاط *+${poin} XP*`)
         } else {
           global.db.data.users[m.sender].exp -= 300
-            m.reply(`▢ *lost*\n\n‣ You : ${text}\n‣ GURU : ${astro}\n\nPoints *-${poin} XP*`)
+            m.reply(`▢ *الخاسر*\n\n‣ انت : ${text}\n‣ تاماكي : ${astro}\n\nالنقاط *-${poin} XP*`)
         }
-    } else if (text == 'paper') {
-        if (astro == 'rock') {
+    } else if (text == 'ورقة') {
+        if (astro == 'حجرة') {
             global.db.data.users[m.sender].exp += 300
-            m.reply(`▢ *won* 🎊\n\n‣ You : ${text}\n‣ GURU : ${astro}\n\n🎁 Points *+${poin} XP*`)
+            m.reply(`▢ *الفائز* 🎊\n\n‣ انت : ${text}\n‣ تاماكي : ${astro}\n\n🎁 النقاط *+${poin} XP*`)
         } else {
           global.db.data.users[m.sender].exp -= 300
-            m.reply(`▢ *You lost*\n\n‣ You : ${text}\n‣ GURU : ${astro}\n\nPoints *-${poin} XP*`)
+            m.reply(`▢ *انت خسرت*\n\n‣ انت : ${text}\n‣ تاماكي : ${astro}\n\nالنقاط *-${poin} XP*`)
         }
     } else {
         throw reseqv
@@ -48,7 +48,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 }
 handler.help = ['ppt <rock/paper/scissors>']
 handler.tags = ['game']
-handler.command = ['ppt'] 
+handler.command = ['ppt','لعبة'] 
 handler.register = false
 
 export default handler
