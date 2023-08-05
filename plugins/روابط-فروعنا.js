789d9af16bb5eccc0e406c063b46a46e2a5c26b6
ᏰᎡᏫᏫᏦ ᏚᎯᎷᎯ <111586917+BrookSama@users.noreply.github.com>
2023-08-05 18:23:42 +0300

    Update روابط-فروعنا.js

--- plugins/روابط-فروعنا.js
+++ plugins/روابط-فروعنا.js
@@ -8,48 +8,48 @@ let handler  = async (m, { conn }) => {
 *┇‌໋☠️ .  - إستقبال انمي⛩️*
 *+120*
 
-``` https://chat.whatsapp.com/IuDAdDRu9GtEStnEwsnmph ```
+ https://chat.whatsapp.com/IuDAdDRu9GtEStnEwsnmph 
 
 *┇☠️ .  - ستيكرات انمي و غيرها🍡*
 *+900*
 
-``` https://chat.whatsapp.com/DV9ME7ocNPJA03qXmezoeI ```
+ https://chat.whatsapp.com/DV9ME7ocNPJA03qXmezoeI 
 
 *┇‌໋☠️ .  - أخبار انمي و المانهاوا🗞️*
 *+450*
 
-``` https://chat.whatsapp.com/GW1od1q3l8i4KLmpsZMHD6 ```
+ https://chat.whatsapp.com/GW1od1q3l8i4KLmpsZMHD6 
 
 *┇‌໋☠️ .  - أفتارات و صور انمي و غيرها📷*
 *+400*
 
-``` https://chat.whatsapp.com/LMiZh36TqPTDAEbDweXoYL ```
+ https://chat.whatsapp.com/LMiZh36TqPTDAEbDweXoYL 
 
 *┇‌໋☠️ . - نشر مغلق📇*
 *+450*
 
-``` https://chat.whatsapp.com/HvF7AJL2A3e35K5eiXwS6l ```
+ https://chat.whatsapp.com/HvF7AJL2A3e35K5eiXwS6l 
 
 *┇‌໋☠️ . - ايديت انمي و غيرها🎥*
 *+400*
 
-``` https://chat.whatsapp.com/CWMFflMoOyR9Y8a5zu4jPV ```
+ https://chat.whatsapp.com/CWMFflMoOyR9Y8a5zu4jPV 
 
 *┇‌໋☠️ . - نشر مفتوح📇*
 *+600*
 
-``` https://chat.whatsapp.com/CaX4tcUUMo1ByEo9e82sw3 ```
+ https://chat.whatsapp.com/CaX4tcUUMo1ByEo9e82sw3 
 
 *┇‌໋☠️ . - نشر مفتوح📇*
 *+100*
 
-``` https://chat.whatsapp.com/HTFzk1rwgpc4ipEUA3QiQk ```
+ https://chat.whatsapp.com/HTFzk1rwgpc4ipEUA3QiQk 
 
 *––––––––– ·「☠️」· –––––––––––*
 
 _*موقع المملكة*_
 
-*『~ https://2u.pw/KR1vZ7Z ~』*
+*『 https://2u.pw/KR1vZ7Z 』*
 
 *––––––––– ·「☠️」· –––––––––––*
 
